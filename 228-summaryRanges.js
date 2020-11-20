58ec172113bd9c5d9d455cd7662b102ec58523ff
Jaspreet Kaur <jassi_sanghera@yahoo.com>
2020-11-20 06:16:56 -0800

    new problem added

---
+++ 228-summaryRanges.js
@@ -0,0 +1,51 @@
+// You are given a sorted unique integer array nums.Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+// Each range [a,b] in the list should be output as:
+// "a->b" if a != b
+// "a" if a == b
+// Example 1:
+// Input: nums = [0,1,2,4,5,7]
+// Output: ["0->2","4->5","7"]
+// Explanation: The ranges are:
+// [0,2] --> "0->2"
+// [4,5] --> "4->5"
+// [7,7] --> "7"
+
+// Example 2:
+// Input: nums = [0,2,3,4,6,8,9]
+// Output: ["0","2->4","6","8->9"]
+// Explanation: The ranges are:
+// [0,0] --> "0"
+// [2,4] --> "2->4"
+// [6,6] --> "6"
+// [8,9] --> "8->9"
+
+// Example 3:
+// Input: nums = []
+// Output: []
+
+// ----------------------------------------------------------------------
+
+var summaryRanges = function(nums) {
+    let result = [];
+    let start = 0;
+    let end = 0;
+    if(nums.length === 0) return result;
+    for(let i =1;i<nums.length+1;i++){
+        if(nums[i] === nums[end]+1){
+            end++;
+        }
+        else{
+            if(start !== end){
+                result.push(`${nums[start]}->${nums[end]}`);
+               start = end + 1;
+               end = start;
+            }
+            else{
+                result.push(`${nums[start]}`);
+               start = end + 1;
+               end = start;
+            }
+    }
+    }
+    return result;
+};
